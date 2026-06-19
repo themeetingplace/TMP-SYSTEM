4f08b1f37b68b6850c46b38cd59fa16996e9a2f0
juukan-bms <fankiki911116@gmail.com>
2026-06-19 12:14:28 +0800

    fix(unsettled): table column widths - 對象 to 22 percent, 銀行末5碼 to 9 percent

--- js/views/unsettled.js
+++ js/views/unsettled.js
@@ -281,12 +281,12 @@ export function renderUnsettled() {
                 <table class="data-table cards-with-hero" style="table-layout: fixed;">
                     <colgroup>
                         <col style="width: 36px;">
-                        <col style="width: 8%;">
-                        <col style="width: 20%;">
+                        <col style="width: 7%;">
                         <col style="width: 14%;">
-                        <col style="width: 11%;">
-                        <col style="width: 10%;">
+                        <col style="width: 22%;">
+                        <col style="width: 14%;">
                         <col style="width: 12%;">
+                        <col style="width: 9%;">
                         <col style="width: 22%;">
                     </colgroup>
                     <thead><tr>
